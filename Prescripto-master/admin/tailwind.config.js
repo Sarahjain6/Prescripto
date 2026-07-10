23733913a40008edd744f9e5e8ae65ae0a7881c6
sarahjain6 <sarahjain62005@gmail.com>
2026-07-11 01:52:10 +0530

    Initial commit

--- Prescripto-master/admin/tailwind.config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: { fontFamily: { sans: ["'Plus Jakarta Sans'", "sans-serif"] } } },
-  plugins: [],
-}
